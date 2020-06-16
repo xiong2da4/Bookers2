e474cf548468b19daed4adddd18080cccf710670
vagrant <vagrant@localhost.localdomain>
2020-06-16 11:51:37 +0000

    [Update]deatil

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
   end
@@ -30,7 +31,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :profile_image_id, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
