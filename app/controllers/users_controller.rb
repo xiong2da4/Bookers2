e0c61e7e8a6b187d1611cd6f1b682ce8e3b4d4dc
vagrant <vagrant@localhost.localdomain>
2020-06-18 05:04:13 +0000

    [Update] left only design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
